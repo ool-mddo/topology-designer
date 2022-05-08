1fab0cf5e75501f66db8c0d9215b21820639826c
shirobrak <shirobrak2810@gmail.com>
2022-05-08 13:00:22 +0900

    Update Create Link Modal

--- src/components/DraftingBoard/CreateLinkModal.tsx
+++ src/components/DraftingBoard/CreateLinkModal.tsx
@@ -1,17 +1,19 @@
+import React, { FC, MouseEvent, useState } from "react";
 import {
   Button,
   Dialog,
+  DialogContent,
   DialogTitle,
   FormControl,
   InputLabel,
   MenuItem,
   Select,
+  Stack,
 } from "@mui/material";
 import Intent, { Node as NodeIntent } from "models/intent";
-import React, { FC, MouseEvent, useState } from "react";
+
 import { useRecoilState, useResetRecoilState } from "recoil";
 import { createLinkState, intentState } from "state";
-
 type Props = {
   isOpen: boolean;
   fromNode: NodeIntent;
@@ -29,14 +31,8 @@ const CreateLinkModal: FC<Props> = ({ isOpen, fromNode, toNode }) => {
     return;
   };
   const onSubmitHandler = () => {
-    console.log(fromInterfaceName);
-    console.log(toInterfaceName);
-    console.log(intent);
-
     const fromInterface = fromNode.findInterface(fromInterfaceName);
     const toInterface = toNode.findInterface(toInterfaceName);
-    console.log(fromInterface);
-    console.log(toInterface);
     if (fromInterface && toInterface) {
       const newIntent = new Intent(intent.id, intent.nodes, intent.links);
       newIntent.addLink(fromInterface, toInterface);
@@ -55,57 +51,72 @@ const CreateLinkModal: FC<Props> = ({ isOpen, fromNode, toNode }) => {
           return;
         }
       }}
+      fullWidth
     >
       <DialogTitle>Create Link</DialogTitle>
-      <FormControl fullWidth>
-        <InputLabel id="select-from-interface-label">From Interface</InputLabel>
-        <Select
-          labelId="select-from-interface-label"
-          label="From Interface"
-          onChange={(e) => {
-            setFromInterfaceName(e.target.value as string);
-          }}
-          value={fromInterfaceName}
-        >
-          {fromNode.interfaces.map((i) => {
-            return (
-              <MenuItem
-                value={i.name}
-                disabled={i.dst() !== undefined}
-                key={`CreateLinkModal-${i.name}`}
-              >
-                {i.name}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="select-to-interface-label">To Interface</InputLabel>
-        <Select
-          label="To Interface"
-          labelId="select-to-interface-label"
-          onChange={(e) => {
-            setToInterfaceName(e.target.value as string);
-          }}
-          value={toInterfaceName}
-        >
-          {toNode.interfaces.map((i) => {
-            console.log(i);
-            return (
-              <MenuItem
-                value={i.name}
-                disabled={i.dst() !== undefined}
-                key={`CreateLinkModal-${i.name}`}
-              >
-                {i.name}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
-      <Button onClick={onSubmitHandler}>Create Link</Button>
-      <Button onClick={onClickCancelBtn}>Cancel</Button>
+      <DialogContent dividers={true}>
+        <Stack spacing={2}>
+          <FormControl fullWidth>
+            <InputLabel id="select-from-interface-label">
+              From Interface
+            </InputLabel>
+            <Select
+              labelId="select-from-interface-label"
+              label="From Interface"
+              onChange={(e) => {
+                setFromInterfaceName(e.target.value as string);
+              }}
+              value={fromInterfaceName}
+            >
+              {fromNode.interfaces.map((i) => {
+                return (
+                  <MenuItem
+                    value={i.name}
+                    disabled={i.dst() !== undefined}
+                    key={`CreateLinkModal-${i.name}`}
+                  >
+                    {i.name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="select-to-interface-label">To Interface</InputLabel>
+            <Select
+              label="To Interface"
+              labelId="select-to-interface-label"
+              onChange={(e) => {
+                setToInterfaceName(e.target.value as string);
+              }}
+              value={toInterfaceName}
+            >
+              {toNode.interfaces.map((i) => {
+                return (
+                  <MenuItem
+                    value={i.name}
+                    disabled={i.dst() !== undefined}
+                    key={`CreateLinkModal-${i.name}`}
+                  >
+                    {i.name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          <Stack
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+            spacing={2}
+          >
+            <Button onClick={onSubmitHandler} variant="contained">
+              Create Link
+            </Button>
+            <Button onClick={onClickCancelBtn}>Cancel</Button>
+          </Stack>
+        </Stack>
+      </DialogContent>
     </Dialog>
   );
 };
